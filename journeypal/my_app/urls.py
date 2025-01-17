2c12f92a8ddf859662ed5f35507298f1c09cf1da
dorkykenny <kennethcarlsamoza8@gmail.com>
2025-01-18 05:16:42 +0800

    adds: bucketlist index and detail templates

--- journeypal/my_app/urls.py
+++ journeypal/my_app/urls.py
@@ -13,5 +13,8 @@ urlpatterns = [
     path('cities/<int:city_id>/add-attraction', views.add_attraction, name='add-attraction'),
     path('cities/<int:city_id>/attractions/<int:attraction_id>/', views.attraction_detail, name='attraction-detail'),
 
+    path('bucketlists/', views.bucketlist_index, name='bucketlist-index'),
+    path('bucketlists/<int:bucketlist_id>/', views.bucketlist_detail, name='bucketlist-detail'),
+
     path('accounts/signup/', views.signup, name='signup'),
 ]
