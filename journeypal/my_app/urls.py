57052e603758ad8ff671da6c690ad8dabc06c457
dorkykenny <kennethcarlsamoza8@gmail.com>
2025-01-17 21:10:01 +0800

    adds: city detail view

--- journeypal/my_app/urls.py
+++ journeypal/my_app/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('cities/', views.city_index, name='city-index'),
+        path('cities/<int:city_id>/', views.city_detail, name='city-detail'),
 ]
--- journeypal/my_app/views.py
+++ journeypal/my_app/views.py
@@ -6,3 +6,7 @@ def home(request):
 def city_index(request):
     cities = City.objects.all()
     return render(request, 'cities/index.html', {'cities': cities})
+
+def city_detail(request, city_id):
+    city = City.objects.get(id=city_id)
+    return render(request, 'cities/detail.html', {'city': city})
