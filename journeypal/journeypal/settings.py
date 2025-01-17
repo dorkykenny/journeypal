5ea7a9d24dcad04282b5065ad81c9a27ffdffbfd
dorkykenny <kennethcarlsamoza8@gmail.com>
2025-01-18 03:47:19 +0800

    adds: protection to routes

--- journeypal/journeypal/settings.py
+++ journeypal/journeypal/settings.py
@@ -117,7 +117,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_URL = 'home'
+
 LOGIN_REDIRECT_URL = 'city-index'
+
 LOGOUT_REDIRECT_URL = 'home'
 
 # Default primary key field type
--- journeypal/my_app/views.py
+++ journeypal/my_app/views.py
@@ -9,19 +9,24 @@ from .forms import AttractionForm
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 class Home(LoginView):
     template_name = 'home.html'
 
+@login_required
 def city_index(request):
     cities = City.objects.all()
     return render(request, 'cities/index.html', {'cities': cities})
 
+@login_required
 def city_detail(request, city_id):
     city = City.objects.get(id=city_id)
     attraction_form = AttractionForm()
     return render(request, 'cities/detail.html', {'city': city, 'attraction_form': attraction_form})
 
-class CityCreate(CreateView):
+class CityCreate(CreateView, LoginRequiredMixin):
     model = City
     fields = ['name', 'country', 'description']
 
@@ -29,14 +34,15 @@ class CityCreate(CreateView):
         form.instance.user = self.request.user
         return super().form_valid(form)
 
-class CityUpdate(UpdateView):
+class CityUpdate(UpdateView, LoginRequiredMixin):
     model = City
     fields = ['country', 'description']
 
-class CityDelete(DeleteView):
+class CityDelete(DeleteView, LoginRequiredMixin):
     model = City
     success_url = '/cities/'
 
+@login_required
 def add_attraction(request, city_id):
     form = AttractionForm(request.POST)
 
@@ -46,6 +52,7 @@ def add_attraction(request, city_id):
         new_attraction.save()
     return redirect('city-detail', city_id=city_id)
 
+@login_required
 def attraction_detail(request, city_id, attraction_id):
     city = City.objects.get(id=city_id)
     attraction = Attraction.objects.get(id=attraction_id)
@@ -61,7 +68,7 @@ def signup(request):
             return redirect('city-index')
         else:
             error_message = 'Invalid sign up, please try again.'
-            
+
     form = UserCreationForm()
     context = {'form': form, 'error_message': error_message}
     return render(request, 'signup.html', context)
